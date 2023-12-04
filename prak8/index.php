4afecbcd78b4aa6dbcfe6bf15a0ddf741b8ba419
Marcel <66813668+marselferrys@users.noreply.github.com>
2023-12-04 23:49:36 +0700

    Create index.php

---
+++ prak8/index.php
@@ -0,0 +1,64 @@
+<?
+include 'connect.php'
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Praktikum 8 Pemweb</title>
+</head>
+
+<body>
+<!-- Nama: Marchel Ferry Timoteus S -->
+<!-- NIM: 121140195 -->
+<!-- Prodi: Teknik Informatika -->
+
+    <form>
+        <label for="mahasiswa"> Pilih Prodi : </label>
+        <select id="mahasiswa" name="mahasiswa">
+            <option value="">Pilih Prodi...</option>
+            <option value="Biomedis">Biomedis</option>
+            <option value="Fisika">Fisika</option>
+            <option valuie="Teknik Industri"> Teknik Industri</option>
+            <option value="Teknik Informatika">Teknik Informatika</option>
+            <option value="Teknik Material">Teknik Material</option>
+        </select>
+    </form>
+
+    <div id="show-table">
+        <table border="1">
+            <tr>
+                <th>No</th>
+                <th>Nim</th>
+                <th>Nama</th>
+                <th>Prodi</th>
+            </tr>
+        </table>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous">
+        </script>
+
+    <script>
+        $(document).ready(function () {
+            $("#mahasiswa").change(function () {
+                var value = $(this).val();
+                $.ajax({
+                    url: "showdata.php",
+                    type: "GET",
+                    data: "request=" + value,
+                    success: function (data) {
+                        $("#show-table").html(data);
+                    }
+                })
+            });
+        });
+    </script>
+</body>
+
+</html>
